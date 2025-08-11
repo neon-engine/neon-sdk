f3b7c847a7eb8a496bf2124af8d5fe9021e87490
Carlos Chulo <cchulo.dev@gmail.com>
2025-08-10 21:34:03 -0700

    redid mmdebstrap

--- scripts/setup-sysroot.sh
+++ scripts/setup-sysroot.sh
@@ -7,17 +7,8 @@ SYSROOT=${SDK}/sysroots/x86_64-gnu-${DEBIAN_RELEASE}
 RUN mkdir -p "${SYSROOT}"
 
 mmdebstrap --mode=chrootless --variant=minbase \
-    --architectures=amd64 \
-    --keyring=/usr/share/keyrings/debian-archive-keyring.gpg \
-    --setup-hook='mkdir -p "$1/dev"; :> "$1/dev/null"; chmod 666 "$1/dev/null"' \
-    --include=g++,binutils,libc6-dev,linux-libc-dev,pkg-config \
-    ${DEBIAN_RELEASE} "${SYSROOT}" http://deb.debian.org/debian
-chroot "${SYSROOT}" apt-get update
-chroot "${SYSROOT}" apt-get install -y --no-install-recommends \
-      libstdc++-12-dev \
-      binutils \
-      libc6-dev \
-      linux-libc-dev \
-      pkg-config
-chroot "${SYSROOT}" apt-get clean
-rm -rf "${SYSROOT}"/var/lib/apt/lists/*
+  --architectures=amd64 \
+  --keyring=/usr/share/keyrings/debian-archive-keyring.gpg \
+  --setup-hook='mkdir -p "$1/dev"; :> "$1/dev/null"; chmod 666 "$1/dev/null"' \
+  --include=binutils,libc6-dev,linux-libc-dev,libstdc++-12-dev,libgcc-s1,pkg-config \
+  "${DEBIAN_RELEASE}" "${SYSROOT}" http://deb.debian.org/debian
